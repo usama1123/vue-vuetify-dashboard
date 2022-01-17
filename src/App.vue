9856b903dd59c137c1cd4983b53aee61c304c9a0
usama1123 <usama.sillanwali@gmail.com>
2022-01-18 01:07:28 +0500

    Just checking some things

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
+     
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-avatar>
-        <img src="https://cdn.vuetifyjs.com/images/john.jpg"
-        alt="john">
-      </v-avatar>
+     
       <v-navigation-drawer
       app
       absolute
@@ -15,19 +13,27 @@
       v-model="drawer"
       height="885px"
       >
+      
       <v-list-item>
         <v-list-item-content>
+
           <v-avatar>
             <img src="https://cdn.vuetifyjs.com/images/john.jpg"
         alt="john">
+        
           </v-avatar>
         </v-list-item-content>
       </v-list-item>
       <v-divider></v-divider>
       <v-list nav>
-        <v-list-item v-for="item in links" :key="item.text" link>
-          <v-list-item-icon><v-icon>{{item.icon}}</v-icon></v-list-item-icon>
-          <v-list-item-content>{{item.text}}</v-list-item-content>
+        <!-- <v-list-item v-for="item in links" :key="item.text" link> -->
+          <v-list-item link>
+          <v-list-item-icon><v-icon>mdi-account</v-icon></v-list-item-icon>
+          <v-list-item-content>Dashboard</v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-icon><v-icon>mdi-image</v-icon></v-list-item-icon>
+          <v-list-item-content>Sample</v-list-item-content>
         </v-list-item>
       </v-list>
       <!-- <v-list>
@@ -64,11 +70,11 @@ export default {
     ]
     //
   }),
-  watch:{
-    group()
-    {
-      this.drawer=false
-    }
-  }
+  // watch:{
+  //   group()
+  //   {
+  //     this.drawer=false
+  //   }
+  // }
 };
 </script>
